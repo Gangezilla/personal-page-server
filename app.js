190c17c08181ecc870c5cfd6e8f8353c9b8aa5b2
Scott <scottgangemi@gmail.com>
2017-12-30 10:36:24 +1100

    Update page, remove package-lock

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const logger = require('./config/logger');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
@@ -8,10 +7,7 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Express only serves static assets in production
-// if (process.env.NODE_ENV === 'production') {
 app.use(express.static('build'));
-// }
 
 app.use(helmet());
 
